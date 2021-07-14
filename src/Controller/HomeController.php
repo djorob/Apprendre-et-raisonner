c1f623703d7f011ad25164c1f9602aa6714485a1
Djo <djorryrobert@gmail.com>
2021-07-14 22:02:51 +0200

    admin dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="test")
+     * @Route("/home", name="app_home")
      */
     public function index(): Response
     {
